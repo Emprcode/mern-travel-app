801271b4c5bd90ee2b7b14930289fc00e0d91f8b
Narayan Aryal <aryal.coding@gmail.com>
2023-12-27 22:30:37 +1100

    project initilized

---
+++ api/src/model/user/UserSchema.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose'
+
+const UserSchema = new mongoose.Schema({
+    userName:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+
+    photo: {
+      type: String,
+    },
+
+    role: {
+      type: String,
+      default: "user",
+    },
+}, {timestamps: true})
+
+export default mongoose.model("User", UserSchema)
