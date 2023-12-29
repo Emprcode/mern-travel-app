f8b1b0df1e3986b457825e554f955899a4a21a02
Narayan Aryal <aryal.coding@gmail.com>
2023-12-29 21:37:32 +1100

    header created

--- frontend/src/components/layout/header/Header.jsx
+++ frontend/src/components/layout/header/Header.jsx
@@ -1,5 +1,66 @@
 import React from "react";
+import { Button, Container, NavLink, Row } from "react-bootstrap";
+import logo from "../../../assets/images/logo.png";
+import { Link } from "react-router-dom";
+const navbarLinks = [
+  {
+    path: "/home",
+    display: "Home",
+  },
+  {
+    path: "#",
+    display: "About",
+  },
+  {
+    path: "/tours",
+    display: "Tours",
+  },
+];
 
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="header">
+      <Container>
+        <Row>
+          <div className="nav__wrapper d-flex align-items-center justify-content-between">
+            {/* logo */}
+            <div className="logo">
+              <img src={logo} alt="logo" width="350px" />
+            </div>
+            {/* menu */}
+            <div className="navigation">
+              <ul className="menu d-flex align-items-center gap-5">
+                {navbarLinks.map((item, i) => (
+                  <li className="nav__item h5 fw-bold" key={i}>
+                    <NavLink to={item.path}>{item.display}</NavLink>
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            {/* menu-end */}
+            <div className="nav__right d-flex align-items-center gap-4">
+              <div className="nav__btns d-flex align-items-center gap-4">
+                <Button variant="none" className="fw-bold">
+                  <Link to="/login" className=" h4 nav-link">
+                    {" "}
+                    Login
+                  </Link>
+                </Button>
+                <Button variant="warning" className="h4 fw-bold rounded-pill">
+                  <Link to="/login" className="nav-link">
+                    {" "}
+                    Register
+                  </Link>
+                </Button>{" "}
+              </div>
+              <span className="mobile_menu">
+                <i className="fa-solid fa-bars"></i>
+              </span>
+            </div>
+          </div>
+        </Row>
+      </Container>
+    </header>
+  );
 };
