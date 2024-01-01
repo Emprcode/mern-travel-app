1660a434bc93c49d67ebdb3149c481479a39ba60
Narayan Aryal <aryal.coding@gmail.com>
2024-01-01 21:17:59 +1100

    updated header

--- frontend/src/components/layout/header/Header.jsx
+++ frontend/src/components/layout/header/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Container, NavLink, Row } from "react-bootstrap";
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
 import logo from "../../../assets/images/logo.png";
 import { Link } from "react-router-dom";
 const navbarLinks = [
@@ -19,48 +19,35 @@ const navbarLinks = [
 
 export const Header = () => {
   return (
-    <header className="header">
+    <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Row>
-          <div className="nav__wrapper d-flex align-items-center justify-content-between">
-            {/* logo */}
-            <div className="logo">
-              <img src={logo} alt="logo" width="350px" />
-            </div>
-            {/* menu */}
-            <div className="navigation">
-              <ul className="menu d-flex align-items-center gap-5">
-                {navbarLinks.map((item, i) => (
-                  <li className="nav__item" key={i}>
-                    <NavLink to={item.path}>{item.display}</NavLink>
-                  </li>
-                ))}
-              </ul>
-            </div>
-
-            {/* menu-end */}
-            <div className="nav__right d-flex align-items-center gap-4">
-              <div className="nav__btns d-flex align-items-center gap-4">
-                <Button variant="none" className="">
-                  <Link to="/login" className="nav-link">
-                    {" "}
-                    Login
-                  </Link>
-                </Button>
-                <Button variant="warning" className="rounded-pill">
-                  <Link to="/login" className="nav-link">
-                    {" "}
-                    Register
-                  </Link>
-                </Button>{" "}
-              </div>
-              <span className="mobile_menu">
-                <i className="fa-solid fa-bars"></i>
-              </span>
-            </div>
+        <Navbar.Brand href="#home">
+          <div className="logo">
+            <img src={logo} alt="logo" width="320px" />
           </div>
-        </Row>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <ul className="menu d-flex justify-content-center align-items-center gap-5">
+              {navbarLinks.map((item, i) => (
+                <li key={i}>
+                  <Link to={item.path} className="nav-link h4">
+                    {item.display}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+            {/* <Nav.Link href="#home">Home</Nav.Link> */}
+          </Nav>
+          <Nav className="">
+            <Button variant="none">Login</Button>{" "}
+            <Button variant="warning" className="mx-2">
+              Register
+            </Button>{" "}
+          </Nav>
+        </Navbar.Collapse>
       </Container>
-    </header>
+    </Navbar>
   );
 };
