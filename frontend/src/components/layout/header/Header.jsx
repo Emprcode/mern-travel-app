4a654aada35db52086389bbfa0befb7f19720ed3
Narayan Aryal <aryal.coding@gmail.com>
2024-01-02 19:28:26 +1100

    header updated

--- frontend/src/components/layout/header/Header.jsx
+++ frontend/src/components/layout/header/Header.jsx
@@ -1,50 +1,45 @@
-import React from "react";
-import { Button, Container, Nav, Navbar } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 import logo from "../../../assets/images/logo.png";
 import { Link } from "react-router-dom";
-const navbarLinks = [
-  {
-    path: "/home",
-    display: "Home",
-  },
-  {
-    path: "#",
-    display: "About",
-  },
-  {
-    path: "/tours",
-    display: "Tours",
-  },
-];
+import Button from "react-bootstrap/esm/Button";
 
 export const Header = () => {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="md" className="bg-body-tertiary">
       <Container>
         <Navbar.Brand href="#home">
-          <div className="logo">
-            <img src={logo} alt="logo" width="320px" />
+          <div>
+            <img src={logo} alt="logo" width="180px" />
           </div>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <ul className="menu d-flex justify-content-center align-items-center gap-5">
-              {navbarLinks.map((item, i) => (
-                <li key={i}>
-                  <Link to={item.path} className="nav-link h4">
-                    {item.display}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-            {/* <Nav.Link href="#home">Home</Nav.Link> */}
+          <Nav className="ms-auto d-flex justify-content-around gap-5">
+            <Link className="nav-link fw-bold h5" to="home">
+              Home
+            </Link>
+            <Link className="nav-link fw-bold h5" to="about">
+              About
+            </Link>
+            <Link className="nav-link fw-bold h5" to="tours">
+              Tours
+            </Link>
           </Nav>
-          <Nav className="">
-            <Button variant="none">Login</Button>{" "}
-            <Button variant="warning" className="mx-2">
+          <Nav className="ms-auto gap-4 ">
+            <Button variant="none" className="fw-bold h5">
+              {" "}
+              Login
+            </Button>
+            <Button variant="warning" className="fw-bold h5">
+              {" "}
               Register
-            </Button>{" "}
+            </Button>
+            <span className="mx-2">
+              {" "}
+              <i className="fa-solid fa-bars"></i>
+            </span>
           </Nav>
         </Navbar.Collapse>
       </Container>
